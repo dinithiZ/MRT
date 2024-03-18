b877db811ff45506f2ab89d1c9d7803b50df92a1
dinithiZ <dinithitt20@gmail.com>
2024-03-18 17:16:48 +0530

    delete

--- Test1.java
+++
@@ -1,4 +0,0 @@
-import java.io.File;  // Import the File class
-
-File myObj = new File("filename.txt"); // Specify the filename
-If you don't know what a package is, read our Java Packages Tutorial.
