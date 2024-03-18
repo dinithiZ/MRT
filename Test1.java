50c149ae7aae52b734ea0e89fa34dc3cf0a4161a
dinithiZ <dinithitt20@gmail.com>
2024-03-18 16:58:19 +0530

    java files

---
+++ Test1.java
@@ -0,0 +1,4 @@
+import java.io.File;  // Import the File class
+
+File myObj = new File("filename.txt"); // Specify the filename
+If you don't know what a package is, read our Java Packages Tutorial.
